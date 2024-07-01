7bb90e3c78ef350ebbe952ff8acf56c66fd16069
lorenzo <lo_tools@tutanota.com>
2024-07-01 15:46:04 +0200

    sostituita gif caricamento

--- loading.js
+++ loading.js
@@ -1,24 +1,12 @@
 
-$('body').append('<div style="" id="loadingDiv"><div class="loading"><img id="loadingBird" src="" /><h3 id="loadingText">Caricamento...</h3></div></div>');
-$(document).ready(function () { 
+$('body').append('<div style="" id="loadingDiv"><div class="loading"><img id="loadingBird" src="images/loading/rondine.gif" /><h3 id="loadingText">Caricamento...</h3></div></div>');
+$(document).ready(function () {
 	$('html').attr('style', 'overflow-y: hidden;');
-
-	var images = ['images/loading/rondineGiu_.webp', 'images/loading/rondineSu_.webp'];
-	var currentIndex = 0;
-	// $('#loadingBird').load('images/loading/rondineGiu_.webp #loadingBird');
-	// $('#loadingBird').load('images/loading/rondineSu_.webp #loadingBird');
-
-	
-	$('#loadingBird').attr('src', "images/loading/rondine.gif");
-	// Imposta l'intervallo per alternare le immagini ogni 3 secondi (3000 millisecondi)
-	// const intervId = setInterval(function () {
-	// 	$('#loadingBird').attr('src', images[currentIndex]);
-	// 	currentIndex = (currentIndex + 1) % images.length;
-
-	// 	if (document.readyState === 'complete')
-	// 		clearInterval(intervId);
-	// }, 600);
-
+	$('#loadingBird').load('images/loading/rondine.gif #loadingBird');
+});
+$(window).on('load', function() {
+	$('#loadingDiv').hide();
+	$('html').attr('style', 'overflow-y: scroll;');
 });
 	
 	$(window).on('load', function(){
